975d435cc42ceeaa10416128d1f2f5668aa97172
drecinos37 <drecinos@chicagobooth.edu>
2015-12-01 21:57:55 -0600

    Forgot to sync

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,18 @@
 class CalculationsController < ApplicationController
   def loan_payment
 
+
+
+@apr = params["apr"].to_f
+@years = params["years"].to_f
+@principal =params["prinicpal"].to_f
+
+rate = @apr / 12
+nper = @years*12
+
+
+@words = params["user_text"]
+@special = params["user_word"]
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "calculations", :action => "loan_payment" })
+  get("/", { :controller => "calculations", :action => "word_count_form" })
 
-  get("/loan_payment/results", { :controller => "calculations", :action => "loan_payment" })
+  get("/word_count/new",                 { :controller => "calculations", :action => "word_count_form" })
+  get("/word_count/results",             { :controller => "calculations", :action => "word_count" })
+
+  get("/loan_payment/new",               { :controller => "calculations", :action => "loan_payment_form" })
+  get("/loan_payment/results",           { :controller => "calculations", :action => "loan_payment" })
+
+  get("/time_between/new",               { :controller => "calculations", :action => "time_between_form" })
+  get("/time_between/results",           { :controller => "calculations", :action => "time_between" })
+
+  get("/descriptive_statistics/new",     { :controller => "calculations", :action => "descriptive_statistics_form" })
+  get("/descriptive_statistics/results", { :controller => "calculations", :action => "descriptive_statistics" })
 
 end
